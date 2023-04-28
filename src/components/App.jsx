de94470cfec83e38d2e8ffaf7f855d35efd03c67
Dmytro Nahornyi <d3049999@gmail.com>
2023-04-28 14:37:32 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { ContactForm } from './phonebook/ContactForm/ContactForm';
+import { ContactList } from './phonebook/ContactList/ContactList';
+import { Filter } from './phonebook/Filter/Filter';
+import { getContactsThunk } from '../redux/contactsThunk';
+import { selectContacts } from '../redux/selectors';
+import { AppContainer, Title } from './phonebook/Phonebook.styled';
+
+export function App() {
+  const contacts = useSelector(selectContacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContactsThunk());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <AppContainer>
+      <Title>Phonebook</Title>
+      <ContactForm />
+      <Filter />
+      {contacts.length > 0 && (
+        <>
+          <h2>Contacts</h2>
+          <ContactList />
+          <ToastContainer />
+        </>
+      )}
+    </AppContainer>
   );
-};
+}
